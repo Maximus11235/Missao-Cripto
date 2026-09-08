ba640a1f11f6af3934266130340f19ca87b39dd7
Kaique-S20 <kaiquestefani03@gmail.com>
2026-09-08 08:53:08 -0300

    teste atualizado

--- tests/testes.py
+++ tests/testes.py
@@ -1,7 +1,8 @@
 # tests/testes.py
 
 # Veja como a importação fica limpa!
-from funcoes import eh_primo, inverso_multiplicativo, euclides_estendido
+from funcoes import (eh_primo, inverso_multiplicativo, euclides_estendido,
+mdc, calcularPhi, expModular)
 
 print("Testando a biblioteca:")
 
@@ -10,3 +11,15 @@ print("103 é primo?", eh_primo(103))
 
 # Testando o inverso multiplicativo
 print("Inverso de 3 mod 11:", inverso_multiplicativo(3, 11))
+
+# Testando o MDC
+print("MDC de 48 e 18:", MDC(48, 18))
+
+# Testando a função calcularPhi
+print("Phi de 10:", calcularPhi(10))
+
+# Testando a exponenciação modular
+print("Exponenciação de 5^3 mod 13:", expModular(5, 3, 13))
+
+# Testando o teorema chinês do resto (ex: x ≡ 2 mod 3 e x ≡ 3 mod 5)
+print("Teorema Chinês do Resto:", teoremaChinesResto([2, 3], [3, 5]))
